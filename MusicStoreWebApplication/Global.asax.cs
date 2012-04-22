5a3513defa10f73b6b33a36c6d00a5a747b8f5c0
Yevgeny <yev@asu.edu>
2012-04-21 21:04:23 -0700

    add a global event to count visitors per app run

--- MusicStoreWebApplication/Global.asax.cs
+++ MusicStoreWebApplication/Global.asax.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Data.SqlClient ;
 using System.Web;
 using System.Web.Security;
 using System.Web.SessionState;
@@ -9,16 +9,31 @@ namespace MusicStoreWebApplication
 {
     public class Global : System.Web.HttpApplication
     {
+        static int visitorCount;
 
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
 
+            visitorCount = 0;
         }
 
         void Application_End(object sender, EventArgs e)
         {
             //  Code that runs on application shutdown
+
+            System.Configuration.Configuration rootWebConfig =
+                System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration("~/");
+            SqlConnection conn =
+                new SqlConnection(rootWebConfig.ConnectionStrings.ConnectionStrings["ApplicationServices"].ToString());
+
+            conn.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = String.Format("INSERT INTO  Visits VALUES ({0})",
+                    visitorCount);
+
+            cmd.Connection = conn;
+            cmd.ExecuteNonQuery(); // Execute the command
 
         }
 
@@ -28,19 +43,15 @@ namespace MusicStoreWebApplication
             Response.Redirect("~/ErrorPage.aspx");
         }
 
-        void Session_Start(object sender, EventArgs e)
+        void Session_OnStart(object sender, EventArgs e)
         {
-            // Code that runs when a new session is started
+            visitorCount++;
 
         }
 
-        void Session_End(object sender, EventArgs e)
+        void Session_OnEnd(object sender, EventArgs e)
         {
-            // Code that runs when a session ends. 
-            // Note: The Session_End event is raised only when the sessionstate mode
-            // is set to InProc in the Web.config file. If session mode is set to StateServer 
-            // or SQLServer, the event is not raised.
-
+            
         }
 
     }
